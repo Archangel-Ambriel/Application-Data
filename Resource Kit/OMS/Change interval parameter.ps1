e672e71d07cbb71838ac73d6f197ecf72ae8824d
༺࿇E.S.A࿇༻ ོSekHmེeT➰ <protection@firewicca.com>
2020-09-14 14:29:24 +0200

    Veeam.Virt.Extensions.HyperV.Analysis.Reports.mp Veeam.Virt.Extensions.HyperV.CapacityPlanning.Reports.mp Veeam.Virt.Extensions.HyperV.Discovery.mp
    Veeam.Virt.Extensions.HyperV.Analysis.Reports.mp
    Veeam.Virt.Extensions.HyperV.CapacityPlanning.Reports.mp
    Veeam.Virt.Extensions.HyperV.Discovery.mp

---
+++ Resource Kit/OMS/Change interval parameter.ps1
@@ -0,0 +1,14 @@
+﻿Param(
+[Parameter(Mandatory=$true)]
+[ValidateRange(1,[int32]::MaxValue)]
+[Int]
+$timeinminutes)
+$filesuffix = "("+$timeinminutes+" minutes)"
+$viewfiles = @("\\Veeam Backup Morning Coffee OMS.omsview","\\Veeam Health Morning Coffee OMS.omsview","\\Veeam Hyper-V Morning Coffee OMS.omsview","\\Veeam VMware Morning Coffee OMS.omsview");
+$invocation = (Get-Variable MyInvocation).Value
+$directorypath = Split-Path $invocation.MyCommand.Path
+foreach ($f in $viewfiles){
+	$filepath = $directorypath+"\\"+$f;	
+	$content = [System.IO.File]::ReadAllText($filepath).Replace("TimeGenerated:[NOW-15minutes..NOW]","TimeGenerated:[NOW-"+$timeinminutes+"minutes..NOW]")
+	[System.IO.File]::WriteAllText($filepath+$filesuffix, $content)
+}
